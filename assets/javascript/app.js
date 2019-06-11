a771ec8f8ae83450d34146066c373159cfd333af
Alex B <ajbarbati@gmail.com>
2019-06-10 22:44:42 -0400

    started arrays and vars

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,76 @@
 
 // running timer
 
+// post questions to page
+var questionArray = ["When was The Lego company founded?","Lego come from the Danish term 'LEg GOdt.' What do these two words mean in English?",
+                        "Which Lego toy for children under the age of five was invented in 1967?", "What is the name of the original Lego theme park in California?",
+                        "Which of the following people has had a Lego character made to look like them?", "Which of these is not a Lego series?", 
+                        "In which of these themes would you find Lego Alpha Team?", "How many pieces come in a standard five-hundred piece bucket?",
+                        "Fortune magazine gave Lego what distinction?", "Which of these is a popular Lego competitor?"
+                    ]
+var answers = {
+    q1: {
+        a: 1937,
+        b: 1932,
+        c: 1951,
+        d: 1926,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+    q1: {
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+    }
+}
+
 // have a function to collect answers
 
 // done button 
