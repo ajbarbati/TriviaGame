565f2fb0816909973c029311bce76ef2c0e13c2d
Alex B <ajbarbati@gmail.com>
2019-06-10 22:24:08 -0400

    finished css laid out javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// learn how to make a start button
+
+// running timer
+
+// have a function to collect answers
+
+// done button 
+
+// takes you to game end screen shows you answers
+
+// correct answers
+
+// incorrect answers
+
+// unanswered questions
