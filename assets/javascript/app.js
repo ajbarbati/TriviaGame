e894279d975c7a459d060d290c6c8706dcede29b
Alex B <ajbarbati@gmail.com>
2019-06-12 21:29:29 -0400

    finished example question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // learn how to make a start button
-
+$(document)
 // running timer
 
 // post questions to page
@@ -15,60 +15,60 @@ var answers = {
         b: 1932,
         c: 1951,
         d: 1926,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
-    }
-    q1: {
-        a: ,
-        b: ,
-        c: ,
-        d: ,
+    },
+    q2: {
+        a: "play children",
+        b: "play well",
+        c: "building for kids",
+        d: "go higher",
+    },
+    q3: {
+        a: "Dacta",
+        b: "Duplo",
+        c: "Le-Baby",
+        d: "Grabbo",
+    },
+    q4: {
+        a: "Lego Studios",
+        b: "Legoworld",
+        c: "Legoland",
+        d: "Lego Adventure Park",
+    },
+    q5: {
+        a: "Steven Spielberg",
+        b: "Rosie O'Donnel",
+        c: "Jim Henson",
+        d: "Dr. Phil",
+    },
+    q6: {
+        a: "Fighter Planes",
+        b: "Paradisa",
+        c: "Harry Potter",
+        d: "Jack Stone",
+    },
+    q7: {
+        a: "The Purple Planet",
+        b: "Underwater",
+        c: "The Center of the Earth",
+        d: "The Ancient Orient",
+    },
+    q8: {
+        a: 500,
+        b: 1000,
+        c: 250,
+        d: 400,
+    },
+    q9: {
+        a: "The Best Toy in the World",
+        b: "Building Smarter Kids Award",
+        c: "Quality, Value< Standards Award",
+        d: "Toy of the Century",
+    },
+    q10: {
+        a: "Quincy's Quality Building Toy's",
+        b: "Stacko Blocks",
+        c: "Mega Blocks",
+        d: "Melissa and Doug's Blocks",
     }
 }
 
@@ -76,10 +76,10 @@ var answers = {
 
 // done button 
 
-// takes you to game end screen shows you answers
+// takes you to game end screen shows you this stuff >
 
-// correct answers
+// - correct answers
 
-// incorrect answers
+// - incorrect answers
 
-// unanswered questions
+// - unanswered questions
